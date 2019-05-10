f9d445115c351d8c818fb92263a1c8c8814e4841
sealanguage <elaine.froehlich@gmail.com>
2019-05-09 20:28:22 -0400

    added changes to see if it will post again.

--- makesNewStuffHello/Program.cs
+++ makesNewStuffHello/Program.cs
@@ -8,7 +8,10 @@ namespace makesNewStuffHello
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("using Git for version control");
-            
+            Console.WriteLine("why doesn't this work???");
+            Console.WriteLine("made some progress, it posted now to repost");
+
+
         }
     }
 }
